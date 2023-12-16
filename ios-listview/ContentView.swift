29eb877b77c15b9324227a65ae2d714552660f82
makvarun <126195300+makvarun@users.noreply.github.com>
2023-12-15 18:26:15 -0800

    Initial Commit

---
+++ ios-listview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-listview
+//
+//  Created by Varun Makwana on 12/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-listview/ios_listviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_listviewApp.swift
+//  ios-listview
+//
+//  Created by Varun Makwana on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_listviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
